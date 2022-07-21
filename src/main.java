ed4c8d0256a09d069a3fe4ae451ffedb24cbb811
ReWinD43 <m.enes.colak43@gmail.com>
2022-07-22 00:35:44 +0200

    test

---
+++ src/main.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+
+public class main {
+    public main(){
+        JFrame meinFrame = new JFrame("Main Window");
+        meinFrame.setSize(200,200);
+        meinFrame.setVisible(true);
+    }
+}
